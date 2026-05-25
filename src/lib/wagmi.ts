6ffd3d041319dfbd1e0880bfe5ff1d4c323e354f
ɴᴀᴏᴍɪ ᴛʜᴇʀᴏɴ <ridzcoder@gmail.com>
2026-05-25 21:03:38 +0300

    Create wagmi.ts

---
+++ src/lib/wagmi.ts
@@ -0,0 +1,12 @@
+import { http, createConfig } from 'wagmi';
+import { injected } from 'wagmi/connectors';
+import { celo, celoSepolia } from 'wagmi/chains';
+
+export const config = createConfig({
+  chains: [celo, celoSepolia],
+  connectors: [injected()],
+  transports: {
+    [celo.id]: http(),
+    [celoSepolia.id]: http(),
+  },
+});
