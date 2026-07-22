f661acb69a7103f28ce8382108be44384c6f4931
ɴᴀᴏᴍɪ ᴛʜᴇʀᴏɴ <ridzcoder@gmail.com>
2026-07-22 22:50:27 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import ProfileSection from '@/components/features/ProfileSection';
 import TechStackSection from '@/components/features/TechStackSection';
-import ProjectSection from '@/components/features/ProjectSection';
+import ProjectsSection from '@/components/features/ProjectSection';
 import ContactSection from '@/components/features/ContactSection';
 import Footer from '@/components/layout/Footer';
 import MenuButton from '@/components/layout/MenuButton';
