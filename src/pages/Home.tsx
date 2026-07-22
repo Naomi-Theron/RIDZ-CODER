4e653888c19ed0dbbb1538d298a710ba0db2f747
ɴᴀᴏᴍɪ ᴛʜᴇʀᴏɴ <ridzcoder@gmail.com>
2026-07-22 22:04:23 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,24 @@
-import Scene3D from '@/components/features/Scene3D';
-import MenuButton from '@/components/layout/MenuButton';
 import ProfileSection from '@/components/features/ProfileSection';
-import SocialLinks from '@/components/features/SocialLinks';
-import StatsSection from '@/components/features/StatsSection';
 import TechStackSection from '@/components/features/TechStackSection';
-import ProjectsSection from '@/components/features/ProjectsSection';
+import ProjectSection from '@/components/features/ProjectSection';
 import ContactSection from '@/components/features/ContactSection';
 import Footer from '@/components/layout/Footer';
+import MenuButton from '@/components/layout/MenuButton';
+import Scene3D from '@/components/features/Scene3D';
 
 export default function Home() {
   return (
     <div className="relative min-h-screen">
       <Scene3D />
       <MenuButton />
-      <main className="relative z-10 max-w-xl mx-auto pb-4">
+
+      <div className="relative z-10">
         <ProfileSection />
-        <SocialLinks />
-        <StatsSection />
         <TechStackSection />
-        <ProjectsSection />
+        <ProjectSection />
         <ContactSection />
         <Footer />
-      </main>
+      </div>
     </div>
   );
 }
