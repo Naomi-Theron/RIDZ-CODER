1eccfda6e13990a2404cbcb190743ea44864f061
ɴᴀᴏᴍɪ ᴛʜᴇʀᴏɴ <ridzcoder@gmail.com>
2026-07-22 22:14:33 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,12 @@ import ProjectSection from '@/components/features/ProjectSection';
 import ContactSection from '@/components/features/ContactSection';
 import Footer from '@/components/layout/Footer';
 import MenuButton from '@/components/layout/MenuButton';
-import Scene3D from '@/components/features/Scene3D';
+import VantaGlobeBackground from '@/components/features/VantaGlobeBackground';
 
 export default function Home() {
   return (
     <div className="relative min-h-screen">
-      <Scene3D />
+      <VantaGlobeBackground />
       <MenuButton />
 
       <div className="relative z-10">
